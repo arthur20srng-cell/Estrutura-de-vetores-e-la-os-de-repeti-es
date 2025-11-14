099758367c9e471812b17d206af00f967dd08a04
Arthur Vinicius Lima de Paula <arthur20srng@gmail.com>
2025-11-14 17:18:00 -0300

    Exemplo de vetor 2.cpp

---
+++ Exemplo de vetor 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string texto[7] = {"Monique", "Ana", "Lucas", "Aiko", "Coaio", "Miguel", "Five"};
+    
+    cout << texto[6];
+
+    return 0;
+}
