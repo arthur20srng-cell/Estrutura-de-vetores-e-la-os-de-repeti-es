1f6db65f6d537bf4f882a2b675152995154c7053
Arthur Vinicius Lima de Paula <arthur20srng@gmail.com>
2025-11-14 17:16:58 -0300

    exemplo de vetor 1.cpp

---
+++ exemplo de vetor 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declaração de um vetor
+    int notas[5] = {1,2,3,4,5};
+    
+    //Acessando um índice do vetor
+    cout << notas{5};
+
+    return 0;
+}
